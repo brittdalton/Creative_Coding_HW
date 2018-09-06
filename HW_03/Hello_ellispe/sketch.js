79601661ad32ed60070087e51f381c7d54d6e84b
brittdalton <brittneydalton@gmail.com>
2018-09-06 16:14:13 -0600

    playing with ellipses

--- HW_03/Hello_ellispe/sketch.js
+++ HW_03/Hello_ellispe/sketch.js
@@ -7,6 +7,7 @@ function draw(){
   ellipse( 20,20,30,30 ); // testing parameters in draw function
                           // ellipse parameter: (x,y,w,h)
   ellipse( 60,60,30,30 ); // adding second ellipse to test x,y placement
-  ellipse( 100,100,30,30 );
-
+  ellipse( 100,100,30,30 ); // attempting to create line of ellipses, they were cut off so i am assuming the default canvas size is too small
+                              // TODO try this again after canvas lesson to see if i can continue the line of ellipses
+  ellipse( 20,20,60,60 );
 }
