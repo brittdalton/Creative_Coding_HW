3b6137b9237871c477de9878bdb7d8e6afa154cc
brittdalton <brittneydalton@gmail.com>
2018-09-06 16:35:45 -0600

    adding notes

--- HW_03/Hello_ellispe/sketch.js
+++ HW_03/Hello_ellispe/sketch.js
@@ -13,9 +13,9 @@ function draw(){
   ellipseMode(CENTER);
   fill(100);
   ellipse(20,20,30,30); // trying out code from p5js.org reference ellipseMode()
-  
+
   ellipse( 60,60,30,30 ); // adding second ellipse to test x,y placement
-  ellipse( 100,100,30,30 ); // attempting to create line of ellipses, they were cut off so i am assuming the default canvas size is too small
+  ellipse( 100,100,30,30 ); // attempting to create line of ellipses, they were cut off so i am assuming the default canvas size is too small, the position from 20,20 to 60,60 was just a random number but i wanted equal spacing so next went to 100,100 and was cut off so it must have a canvas of similar size
                               // TODO try this again after canvas lesson to see if i can continue the line of ellipses
 
 }
