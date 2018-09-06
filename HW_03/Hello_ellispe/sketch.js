5466d8346a1b3b51089ceb0e1a5b347e0b99ad51
brittdalton <brittneydalton@gmail.com>
2018-09-06 16:29:48 -0600

    ellipseMode

--- HW_03/Hello_ellispe/sketch.js
+++ HW_03/Hello_ellispe/sketch.js
@@ -4,10 +4,18 @@ function setup(){     // setup funtion needs to be in the top of each sketch.js
 }
 
 function draw(){
-  ellipse( 20,20,30,30 ); // testing parameters in draw function
+//  ellipse( 20,20,30,30 ); // testing parameters in draw function
                           // ellipse parameter: (x,y,w,h)
+  ellipseMode(RADIUS);
+  fill(255);
+  ellipse (20,20,30,30);
+
+  ellipseMode(CENTER);
+  fill(100);
+  ellipse(20,20,30,30); // trying out code from p5js.org reference ellipseMode()
+  
   ellipse( 60,60,30,30 ); // adding second ellipse to test x,y placement
   ellipse( 100,100,30,30 ); // attempting to create line of ellipses, they were cut off so i am assuming the default canvas size is too small
                               // TODO try this again after canvas lesson to see if i can continue the line of ellipses
-  ellipse( 20,20,60,60 );
+
 }
