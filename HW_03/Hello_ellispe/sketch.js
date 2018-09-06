868c248fd45a3e20035af51f9c7bfe5528c78990
brittdalton <brittneydalton@gmail.com>
2018-09-06 15:04:53 -0600

    change ellipse x,y

--- HW_03/Hello_ellispe/sketch.js
+++ HW_03/Hello_ellispe/sketch.js
@@ -1,7 +1,11 @@
-function setup(){
+function setup(){     // setup funtion needs to be in the top of each sketch.js
+                      // and only needs to be added once
 
 }
 
 function draw(){
-  ellipse( 20,20,30,30 );
+  ellipse( 20,20,30,30 ); // testing parameters in draw function
+                          // ellipse parameter: (x,y,w,h)
+  ellipse( 60,60,30,30 );
+
 }
