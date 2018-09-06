9ce4d1fd65a5b45473774714c5a276e9c06e929f
brittdalton <brittneydalton@gmail.com>
2018-09-06 15:06:05 -0600

    third ellipse

--- HW_03/Hello_ellispe/sketch.js
+++ HW_03/Hello_ellispe/sketch.js
@@ -6,6 +6,7 @@ function setup(){     // setup funtion needs to be in the top of each sketch.js
 function draw(){
   ellipse( 20,20,30,30 ); // testing parameters in draw function
                           // ellipse parameter: (x,y,w,h)
-  ellipse( 60,60,30,30 );
+  ellipse( 60,60,30,30 ); // adding second ellipse to test x,y placement
+  ellipse( 100,100,30,30 );
 
 }
