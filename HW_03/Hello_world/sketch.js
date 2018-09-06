d0c5df61fcee882e48d1e3c4078666b490ca72e3
brittdalton <brittneydalton@gmail.com>
2018-09-06 17:48:08 -0600

    adjusting text

--- HW_03/Hello_world/sketch.js
+++ HW_03/Hello_world/sketch.js
@@ -1,10 +1,14 @@
 function setup(){
-  createCanvas( 400,400 )
-  background ( 'pink' )
+  createCanvas( 400,400 ) // I is making a canvas
+  background ( 'pink' ) // I is coloring it pink
 
 }
 
 function draw (){
-         text ("Hello Universe!",0,10);
+         text ("Hello Universe!",0,40);
+         /* saying hello to the universe, extending greetings beyond
+         planet earth (just in case)
+         */
+
 
 }
