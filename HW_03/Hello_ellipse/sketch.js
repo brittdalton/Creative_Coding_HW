ae193262b475e025c9fbe8340a7a766400025ba4
brittdalton <brittneydalton@gmail.com>
2018-09-06 23:50:58 -0600

    change line

--- HW_03/Hello_ellipse/sketch.js
+++ HW_03/Hello_ellipse/sketch.js
@@ -39,6 +39,6 @@ function draw(){
   ellipse( 280,300,30,30 ); //test to change direction
   ellipse( 300,340,30,30 ); //creating dropping effect
 
-  line (0,0,400,400)
+  line (400,400,0,400)
   stroke ('pink')
 }
