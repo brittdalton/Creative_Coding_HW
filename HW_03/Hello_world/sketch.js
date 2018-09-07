3940352153dc8caf7cd664c3273ae96b75114d02
brittdalton <brittneydalton@gmail.com>
2018-09-06 21:08:49 -0600

    changes

--- HW_03/Hello_world/sketch.js
+++ HW_03/Hello_world/sketch.js
@@ -1,11 +1,11 @@
-function setup(){
-  createCanvas( 400,400 ) // I is making a canvas
-  background ( 'pink' ) // I is coloring it pink
+function setup(){  //setup document
+  createCanvas( 400,400 ) // I is making a canvas...creating canvas to work on
+  background ( 'pink' ) // I is coloring it pink....coloring background pink
 
 }
 
-function draw (){
-         text ("Hello Universe!",40,40);
+function draw (){       //instruction to draw
+         text ("Hello Universe!",40,40);  // text to draw on canvas
          /* saying hello to the universe, extending greetings beyond
          planet earth (just in case)
          */
