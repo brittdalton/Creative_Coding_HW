0cdb73678169ce765f5ad34e3d34d74cb1ef41a6
brittdalton <brittneydalton@gmail.com>
2018-09-20 01:53:07 -0600

    canvas size

--- HW_04/sketch.js
+++ HW_04/sketch.js
@@ -1,5 +1,5 @@
 function setup(){     // setup funtion needs to be in the top of each sketch.js
-createCanvas( 600,600 )        // and only needs to be added once
+createCanvas( 900,900 )        // and only needs to be added once
 background ( 'rgba(199, 109, 36, 0.91)' );
 
 }
