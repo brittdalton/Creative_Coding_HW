69a2ca28139894ee0b954fea594d2746e0e7385a
brittdalton <brittneydalton@gmail.com>
2018-09-20 05:26:27 -0600

    last save night

--- HW_04/sketch.js
+++ HW_04/sketch.js
@@ -1,11 +1,25 @@
 function setup(){     // setup funtion needs to be in the top of each sketch.js
 createCanvas( 900,900 )        // and only needs to be added once
-background ( 'rgba(199, 109, 36, 0.91)' );
+
 
 }
 
 
 
 function draw(){
+
+background ( 'rgba(199, 109, 36, 0.91)' ); //background color
+
+
+push();                                     //sandbox creation
+translate(450,450);                        //alter center point
+pop();
+
+
+push();                                       //Body
+
+
+pop();
+  
 
 }
